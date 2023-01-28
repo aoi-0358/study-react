ca7b6e51b80e63511f9cd7470b7cdfd09d94bdb4
aoi-0358 <aogangcun@gmail.com>
2023-01-28 16:20:34 +0900

    ✨コンポーネント追加

---
+++ components/Headline.js
@@ -0,0 +1,16 @@
+import { Inter } from '@next/font/google'
+import styles from '@/styles/Home.module.css'
+
+
+const inter = Inter({ subsets: ['latin'] })
+
+export default function Headline(props) {
+  return (
+    <>
+          <p>
+              {props.page}.js started by editing&nbsp;
+            <code className={styles.code}>{props.page} page</code>
+          </p>
+    </>
+  )
+}
