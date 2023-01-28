a004721e3f3a8d5d4cb08265823c7d0b9cb9026b
aoi-0358 <aogangcun@gmail.com>
2023-01-28 16:29:32 +0900

    ♻️ export defaultの削除

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,7 +4,7 @@ import { Inter } from '@next/font/google'
 import styles from '@/styles/Home.module.css'
 import { Main } from 'components/main'
 import { FooterLinks } from 'components/FooterLinks'
-import Headline from 'components/Headline'
+import { Headline } from 'components/Headline'
 
 const inter = Inter({ subsets: ['latin'] })
 
