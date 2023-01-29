3e47a8613a43c585bd0516236ae70361b99db9b9
aoi-0358 <aogangcun@gmail.com>
2023-01-29 17:43:48 +0900

    ⚗️ 不要な記述でいつか消すしますが、試しにクリックイベントを書いてみました

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -6,10 +6,19 @@ import { FooterLinks } from '@/components/FooterLinks'
 import { Headline } from '@/components/Headline'
 import { Img } from '@/components/Img'
 import { Header } from '@/components/Header'
+import { useCallback } from 'react'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
+  const foo = 1;
+   
+  const handleClick = useCallback((e) => {
+    console.log(e.target.href);
+    e.preventDefault();  
+    alert(foo);
+   }, []);
+
   return (
     <>
       <Head>
@@ -17,6 +26,9 @@ export default function Home() {
       </Head>
 
       <Header />
+      <a href="/about" onClick={handleClick}>
+        ボタン
+      </a>
 
       <main className={styles.main}>
         <div className={styles.description}>
