a8e666d6efca485e956548f204f2ec5f3c799717
aoi-0358 <aogangcun@gmail.com>
2023-01-29 18:26:50 +0900

    ✨講座用のuseEffectを導入

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,7 +1,16 @@
 import Link from "next/link";
 import classes from "src/components/Header/Header.module.css";
+import { useEffect } from "react";
 
 export function Header () {
+  useEffect(() => {
+    console.log("マウント時");
+    document.body.style.backgroundColor = "lightblue";
+    return () => {
+      console.log("アンマウント時");
+      document.body.style.backgroundColor = "";
+     };
+  }, []);
   return(
     <>
      <header className={classes.header}>
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -6,30 +6,27 @@ import { FooterLinks } from '@/components/FooterLinks'
 import { Headline } from '@/components/Headline'
 import { Img } from '@/components/Img'
 import { Header } from '@/components/Header'
-import { useCallback } from 'react'
+import { useCallback, useEffect } from 'react'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
-  const foo = 1;
-   
-  const handleClick = useCallback((e) => {
-    console.log(e.target.href);
-    e.preventDefault();  
-    alert(foo);
-   }, []);
+  // const foo = 1;
+  // const handleClick = useCallback((e) => {
+  //   console.log(e.target.href);
+  //   e.preventDefault();  
+  //   alert(foo);
+  //  }, []);
 
   return (
     <>
       <Head>
         <title>Index Page</title>
-      </Head>
-
+      </Head>       
       <Header />
-      <a href="/about" onClick={handleClick}>
+      {/* <a href="/about" onClick={handleClick}>
         ボタン
-      </a>
-
+      </a> */}
       <main className={styles.main}>
         <div className={styles.description}>
           <Headline 
