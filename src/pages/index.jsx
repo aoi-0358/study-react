3c735087234aa92d6ddcbd312cd69793e1f4cad7
aoi-0358 <aogangcun@gmail.com>
2023-01-30 20:48:32 +0900

    ✨useStateで文字列と真偽値について学習

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -11,15 +11,29 @@ import { useEffect } from 'react'
 import { useCallback } from 'react'
 
 const inter = Inter({ subsets: ['latin'] })
-
 export default function Home() {
   const [count, setCount] = useState(1);
+  const [text, setText] = useState("");
+  const [isShow, setIsShow] = useState(true);
+
   const handleClick = useCallback(() => {
     console.log(count);
     if (count < 10) {
     setCount((count) => count + 1);
     }
    }, [count]);
+
+   const handleDisplay = useCallback(() => {
+    setIsShow((isShow) => !isShow);
+   }, []);
+
+   const handleChange = useCallback((e) => {
+    if (e.target.value.length > 5) {
+      alert("五文字以内");
+      return;
+    }
+    setText(e.target.value.trim())
+  }, []);
 
    useEffect(() => {
     document.body.style.backgroundColor = "lightblue";
@@ -34,8 +48,21 @@ export default function Home() {
         <title>Index Page</title>
       </Head>       
       <Header />
-      <h1>{count}</h1>
+
+
+      {isShow ? <h1>{count}</h1> : null}
       <button onClick={handleClick}>ボタン</button>
+      <button onClick={handleDisplay}>
+       {isShow ? "非表示" : "表示"}
+      </button>
+
+
+  
+      <input 
+      type="text"  
+      value={text} 
+      onChange={handleChange} />
+
       <main className={styles.main}>
         <div className={styles.description}>
           <Headline 
