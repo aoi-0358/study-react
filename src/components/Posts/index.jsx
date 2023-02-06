28e2afc3c531647adf4a1b67a92a61ac061127a9
aoi-0358 <aogangcun@gmail.com>
2023-02-07 05:50:04 +0900

    ⚡️useReducerを使った

--- src/components/Posts/index.jsx
+++ src/components/Posts/index.jsx
@@ -1,16 +1,39 @@
 import { Inter } from "@next/font/google";
 import styles from "@/styles/Home.module.css";
-import { useState } from "react";
-import { useCallback } from "react";
-import { useEffect } from "react";
+import { useCallback, useEffect, useReducer } from "react";
 
 const inter = Inter({ subsets: ["latin"] });
 
+const initialState = {
+  data: [],
+  loading: true,
+  error: null,
+};
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "end":
+      return {
+        ...state,
+        data: action.data,
+        loading: false,
+      };
+
+    case "error":
+      return {
+        ...state,
+        loading: false,
+        error: action.error,
+      };
+
+    default:
+      throw new Error("no such action type!");
+  }
+};
+
 //--------------------home
 export const Posts = () => {
-  const [posts, setposts] = useState([]);
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(null);
+  const [state, dispatch] = useReducer(reducer, initialState);
 
   const getPosts = useCallback(async () => {
     try {
@@ -19,34 +42,32 @@ export const Posts = () => {
         throw new Error("エラーが発生したため、データの取得に失敗しました。");
       }
       const json = await res.json();
-      setposts(json);
+      dispatch({ type: "end", data: json });
     } catch (error) {
-      setError(error);
+      dispatch({ type: "error", error });
     }
-
-    setLoading(false);
   }, []);
 
   useEffect(() => {
     getPosts();
   }, [getPosts]);
 
-  if (loading) {
+  if (state.loading) {
     return <div>ローディング中</div>;
   }
 
-  if (error) {
-    return <div>{error.message}</div>;
+  if (state.error) {
+    return <div>{state.error.message}</div>;
   }
 
-  if (posts.length === 0) {
+  if (state.data.length === 0) {
     return <div>データは空です</div>;
   }
 
   return (
     <>
       <ol className={styles.wrapper}>
-        {posts.map((post) => {
+        {state.data.map((post) => {
           return <li key={post.id}>{post.title}</li>;
         })}
       </ol>
