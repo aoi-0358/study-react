5dcf8bddaa235a528535232107e4450b65b9b283
aoi-0358 <aogangcun@gmail.com>
2023-02-09 19:52:55 +0900

    ⚠︎SWRでAPIを取得できない

--- src/components/Posts/index.jsx
+++ src/components/Posts/index.jsx
@@ -1,6 +1,7 @@
 import { Inter } from "@next/font/google";
 import styles from "@/styles/Home.module.css";
 import { useCallback, useEffect, useReducer } from "react";
+import useSWR from "swr";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -33,41 +34,43 @@ const reducer = (state, action) => {
 
 //--------------------home
 export const Posts = () => {
-  const [state, dispatch] = useReducer(reducer, initialState);
+  const { data, error } = useSWR(`https://jsonplaceholder.typicode.com/posts`);
+  console.log({ data, error });
+  // const [state, dispatch] = useReducer(reducer, initialState);
 
-  const getPosts = useCallback(async () => {
-    try {
-      const res = await fetch("https://jsonplaceholder.typicode.com/posts");
-      if (!res.ok) {
-        throw new Error("エラーが発生したため、データの取得に失敗しました。");
-      }
-      const json = await res.json();
-      dispatch({ type: "end", data: json });
-    } catch (error) {
-      dispatch({ type: "error", error });
-    }
-  }, []);
+  // const getPosts = useCallback(async () => {
+  //   try {
+  //     const res = await fetch("https://jsonplaceholder.typicode.com/posts");
+  //     if (!res.ok) {
+  //       throw new Error("エラーが発生したため、データの取得に失敗しました。");
+  //     }
+  //     const json = await res.json();
+  //     dispatch({ type: "end", data: json });
+  //   } catch (error) {
+  //     dispatch({ type: "error", error });
+  //   }
+  // }, []);
 
-  useEffect(() => {
-    getPosts();
-  }, [getPosts]);
+  // useEffect(() => {
+  //   getPosts();
+  // }, [getPosts]);
 
-  if (state.loading) {
+  if (!error && !data) {
     return <div>ローディング中</div>;
   }
 
-  if (state.error) {
-    return <div>{state.error.message}</div>;
+  if (error) {
+    return <div>{error.message}</div>;
   }
 
-  if (state.data.length === 0) {
+  if (data.length === 0) {
     return <div>データは空です</div>;
   }
 
   return (
     <>
       <ol className={styles.wrapper}>
-        {state.data.map((post) => {
+        {data.map((post) => {
           return <li key={post.id}>{post.title}</li>;
         })}
       </ol>
