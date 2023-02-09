34ec98bb6e7330e47d4d77f176c90b3aeca2d6c8
aoi-0358 <aogangcun@gmail.com>
2023-02-09 21:32:47 +0900

    🎨 Replace to swr.

--- src/components/Posts/index.jsx
+++ src/components/Posts/index.jsx
@@ -1,61 +1,14 @@
 import { Inter } from "@next/font/google";
 import styles from "@/styles/Home.module.css";
-import { useCallback, useEffect, useReducer } from "react";
-import useSWR from "swr";
+import { usePosts } from "src/hooks/usePosts";
 
 const inter = Inter({ subsets: ["latin"] });
 
-const initialState = {
-  data: [],
-  loading: true,
-  error: null,
-};
-
-const reducer = (state, action) => {
-  switch (action.type) {
-    case "end":
-      return {
-        ...state,
-        data: action.data,
-        loading: false,
-      };
-
-    case "error":
-      return {
-        ...state,
-        loading: false,
-        error: action.error,
-      };
-
-    default:
-      throw new Error("no such action type!");
-  }
-};
-
 //--------------------home
 export const Posts = () => {
-  const { data, error } = useSWR(`https://jsonplaceholder.typicode.com/posts`);
-  console.log({ data, error });
-  // const [state, dispatch] = useReducer(reducer, initialState);
+  const { data, error, isLoading, isEmpty } = usePosts();
 
-  // const getPosts = useCallback(async () => {
-  //   try {
-  //     const res = await fetch("https://jsonplaceholder.typicode.com/posts");
-  //     if (!res.ok) {
-  //       throw new Error("エラーが発生したため、データの取得に失敗しました。");
-  //     }
-  //     const json = await res.json();
-  //     dispatch({ type: "end", data: json });
-  //   } catch (error) {
-  //     dispatch({ type: "error", error });
-  //   }
-  // }, []);
-
-  // useEffect(() => {
-  //   getPosts();
-  // }, [getPosts]);
-
-  if (!error && !data) {
+  if (isLoading) {
     return <div>ローディング中</div>;
   }
 
@@ -63,7 +16,7 @@ export const Posts = () => {
     return <div>{error.message}</div>;
   }
 
-  if (data.length === 0) {
+  if (isEmpty) {
     return <div>データは空です</div>;
   }
 
