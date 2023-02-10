afbfec82e6bc2caecd303c83491f60294ca22599
aoi-0358 <aogangcun@gmail.com>
2023-02-11 08:14:00 +0900

    ⚠︎SWRでpostの子ページの非同期処理ができない

--- src/pages/post/[id]/index.jsx
+++ src/pages/post/[id]/index.jsx
@@ -3,19 +3,27 @@ import styles from "@/styles/Home.module.css";
 import { Inter } from "@next/font/google";
 import { Header } from "@/components/Header";
 import { useRouter } from "next/router";
+import { useSWR } from "swr";
 
 const inter = Inter({ subsets: ["latin"] });
 
-//--------------------home
 const PostId = () => {
   const router = useRouter();
+  const { data, error } = useSWR(
+    "https://jsonplaceholder.typicode.com/posts/1",fetcher
+  );
+  console.log({ data, error });
+
   return (
     <>
       <Head>
-        <title>Index Page</title>
+        <title>{data?.title}</title>
       </Head>
       <Header />
-      <div>{router.query.id}</div>
+      <div>
+        <h1>{data?.title}</h1>
+        <p>{data?.body}</p>
+      </div>
     </>
   );
 };
