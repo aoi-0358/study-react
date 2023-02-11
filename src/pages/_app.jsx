f2f9f7b7b3e159c0f5dd09e5e832e1abf5f58310
aoi-0358 <aogangcun@gmail.com>
2023-02-11 21:19:48 +0900

    🔥 不必要なコードやファイルを削除

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,14 +1,9 @@
 import "@/styles/globals.css";
 import Head from "next/head";
-import { useCounter } from "@/hooks/useCounter";
-import { useInputArray } from "@/hooks/useInputArray";
-import { useBgColor } from "@/hooks/useBgColor";
+
+
 
 export default function App({ Component, pageProps }) {
-  const counter = useCounter();
-  const InputArray = useInputArray();
-  useBgColor();
-
   return (
     <>
       <Head>
@@ -16,7 +11,7 @@ export default function App({ Component, pageProps }) {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Component {...pageProps} {...counter} {...InputArray} />
+      <Component {...pageProps}/>
     </>
   );
 }
