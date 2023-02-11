1005325b42428602bfcfa9c80d48598e71cb22f1
aoi-0358 <aogangcun@gmail.com>
2023-02-11 21:44:19 +0900

    ♻️ prettierを適用

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -2,24 +2,20 @@ import Link from "next/link";
 import classes from "src/components/Header/Header.module.css";
 
 const NAV_ITEMS = [
-  {href: "/", label: "Index"}, 
-  {href: "/posts", label: "Posts"}, 
+  { href: "/", label: "Index" },
+  { href: "/posts", label: "Posts" },
 ];
 
 export const Header = () => {
-  return(
-    
-     <header className={classes.header}>
+  return (
+    <header className={classes.header}>
       {NAV_ITEMS.map((item) => {
         return (
           <Link key={item.href} href={item.href}>
             <div className={classes.anchor}>{item.label}</div>
-          </Link>  
+          </Link>
         );
       })}
-  
-
-     
-     </header>   
+    </header>
   );
 };
