592462d53d5ef372163ec38810d19d6c15eb9221
aoi-0358 <aogangcun@gmail.com>
2023-02-19 07:06:02 +0900

    ➕ Tailwind Cssのセットアップ

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,5 +1,6 @@
+import "tailwindcss/tailwind.css";
 import { Layout } from "src/components/Layout";
-import "@/styles/globals.css";
+//import "@/styles/globals.css";
 import Head from "next/head";
 
 export default function App({ Component, pageProps }) {
