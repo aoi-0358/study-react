388eb0918c9d8c0466b4a3cf65be6f9578743515
aoi-0358 <aogangcun@gmail.com>
2023-02-20 17:33:51 +0900

    ✨ユーザー詳細ページでSSRを実践

--- src/pages/users/[id]/index.jsx
+++ src/pages/users/[id]/index.jsx
@@ -1,15 +1,31 @@
 import { UserComponent } from "@/components/User";
 import { Inter } from "@next/font/google";
 import { Header } from "src/components/Header";
+import { SWRConfig } from "swr";
 
 const inter = Inter({ subsets: ["latin"] });
 
-const UsersId = () => {
+export const getServerSideProos = async (ctx) => {
+  const { id } = ctx.query;
+  const API_URL = `https://jsonplaceholder.typicode.com/users/${id}`;
+  const user = await fetch(API_URL);
+  const userData = await user.json();
+  return {
+    props: {
+      fallback: {
+        [API_URL]: userData,
+      },
+    },
+  };
+};
+
+const UsersId = (props) => {
+  const { fallback } = props;
   return (
-    <div>
+    <SWRConfig value={{ fallback }}>
       <Header />
       <UserComponent />
-    </div>
+    </SWRConfig>
   );
 };
 
